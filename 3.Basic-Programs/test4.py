93e08106e8d3234500a4a2b14bfed31b8703b9b7
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-10-18 02:20:53 +0530

    test4 solved from hacker-rank

---
+++ 3.Basic-Programs/test4.py
@@ -0,0 +1,11 @@
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+
+a = int(input())
+b = int(input())
+    
+print(f"{a+b}")
+print(f"{a-b}")
+print(f"{a*b}")
